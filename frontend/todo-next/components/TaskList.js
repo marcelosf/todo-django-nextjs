5765c7f586939aa9bfc84456f0ee757150406d75
Marcelo Schneider <schneider.fei@gmail.com>
2021-12-23 19:17:54 -0300

    Render the task list

--- frontend/todo-next/components/TaskList.js
+++ frontend/todo-next/components/TaskList.js
@@ -4,19 +4,26 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function TaskList({ tasks }) {
+function formatDate(date, local) {
+  const formatted = new Date(date + "T00:00:00");
+  return formatted.toLocaleDateString(local);
+}
+
+function TaskList({ tasks }) {
   return (
     <div>
       {tasks.map((task, index) => (
         <Accordion key={index} role="accordion">
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{task.summary}</Typography>
+            <Typography>{task.name}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>{task.detail}</Typography>
+            <Typography>{formatDate(task.date, "pt-BR")}</Typography>
           </AccordionDetails>
         </Accordion>
       ))}
     </div>
   );
 }
+
+export default TaskList;
