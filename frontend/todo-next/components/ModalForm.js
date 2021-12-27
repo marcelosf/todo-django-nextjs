48afd414a5f32129240e1e8d04d2e65aebca8cc4
Marcelo Schneider <schneider.fei@gmail.com>
2021-12-27 00:38:49 -0300

    Submit tasks data

--- frontend/todo-next/components/ModalForm.js
+++ frontend/todo-next/components/ModalForm.js
@@ -12,6 +12,7 @@ import Checkbox from "@mui/material/Checkbox";
 import DatePicker from "@mui/lab/DatePicker";
 import AdapterMoment from "@mui/lab/AdapterMoment";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import moment from "moment";
 
 const modalStyle = {
   position: "absolute",
@@ -22,10 +23,38 @@ const modalStyle = {
 };
 
 export default function ModalForm() {
+  const [name, setName] = React.useState("");
   const [open, setOpen] = React.useState(false);
+  const [done, setDone] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [date, setDate] = React.useState(null);
+
+  function getFormattedDate() {
+    const dateObj = new Date(date);
+    return moment(dateObj).format("YYYY-MM-DD");
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formattedDate = getFormattedDate();
+    const data = JSON.stringify({
+      name: name,
+      date: formattedDate,
+      status: open,
+    });
+
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_URL + "/api/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: data,
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <div>
@@ -33,32 +62,42 @@ export default function ModalForm() {
         Add new task
       </Button>
       <Modal open={open} onClose={handleClose}>
-        <Card sx={modalStyle}>
-          <CardHeader title="New Task" />
-          <CardContent>
-            <Box
-              component="form"
-              sx={{ "& .MuiTextField-root": { mb: 3, width: "100%" } }}
-            >
-              <TextField label="Name" variant="outlined" />
-              <LocalizationProvider dateAdapter={AdapterMoment}>
-                <DatePicker
-                  label="Date"
-                  value={date}
-                  onChange={(newDate) => {
-                    setDate(newDate);
-                  }}
-                  renderInput={(params) => <TextField {...params} />}
+        <form onSubmit={handleSubmit}>
+          <Card sx={modalStyle}>
+            <CardHeader title="New Task" />
+            <CardContent>
+              <Box
+                component="form"
+                sx={{ "& .MuiTextField-root": { mb: 3, width: "100%" } }}
+              >
+                <TextField
+                  label="Name"
+                  variant="outlined"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
-              </LocalizationProvider>
-              <FormControlLabel control={<Checkbox />} label="Done" />
-            </Box>
-          </CardContent>
-          <CardActions>
-            <Button>Add</Button>
-            <Button onClick={handleClose}>Cancel</Button>
-          </CardActions>
-        </Card>
+                <LocalizationProvider dateAdapter={AdapterMoment}>
+                  <DatePicker
+                    label="Date"
+                    value={date}
+                    onChange={(newDate) => {
+                      setDate(newDate);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+                <FormControlLabel
+                  control={<Checkbox value={done} />}
+                  label="Done"
+                />
+              </Box>
+            </CardContent>
+            <CardActions>
+              <Button type="submit">Add</Button>
+              <Button onClick={handleClose}>Cancel</Button>
+            </CardActions>
+          </Card>
+        </form>
       </Modal>
     </div>
   );
